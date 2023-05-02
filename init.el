973e9585023eeaba57fed85992d2fd3774c7b9bb
maximo tejeda <maximotejeda@gmail.com>
2023-05-02 11:01:17 -0400

    config to org-capture and or-refile

--- init.el
+++ init.el
@@ -27,9 +27,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
+ '(company-show-quick-access t nil nil "Customized with use-package company")
+ '(org-agenda-files
+   '("/home/maximo/.org/tasks.org" "/home/maximo/.org/journal.org" "/home/maximo/.org/organizer.org" "/home/maximo/.emacs.d/config.org"))
  '(package-selected-packages
-   '(magit yaml-mode xref-js2 which-key web-mode use-package rjsx-mode rainbow-mode neotree material-theme lsp-ui lsp-ivy js2-refactor helm-lsp gotest go-projectile go-autocomplete flymake-python-pyflakes flymake-golangci flymake-go-staticcheck elpy dap-mode better-defaults)))
+   '(mixed-pitch magit yaml-mode xref-js2 which-key web-mode use-package rjsx-mode rainbow-mode neotree material-theme lsp-ui lsp-ivy js2-refactor helm-lsp gotest go-projectile go-autocomplete flymake-python-pyflakes flymake-golangci flymake-go-staticcheck elpy dap-mode better-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
